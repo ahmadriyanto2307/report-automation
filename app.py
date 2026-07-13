4a5bfb4079c2f1752bde1294092c2e42349b076f
ahmadriyanto2307 <ahmad.riyanto2307@gmail.com>
2026-07-13 11:46:32 +0700

    feat: initialize FastAPI project

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="Report Automation API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {
+        "status": "OK",
+        "project": "Report Automation"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
